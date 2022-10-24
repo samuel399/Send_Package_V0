28e615fbc5cfd05ff78e8a5f716ad66b81042e8e
samuel399 <augisto46@gmail.com>
2022-10-24 18:14:55 -0300

    start conection teste

--- src/main.c
+++ src/main.c
@@ -2,10 +2,39 @@
 #include <stdlib.h>
 #include <Winsock2.h>
 
+#define SERVER_PORT 5040
+
+struct sockaddr_in server_address;
+struct sockaddr_in client_address;
+SOCKET s, new_s;
+
+
+
+
+void Start_Conection(void)
+{
+    WSADATA info;
+
+    if (WSAStartup(MAKEWORD(1,1), &info) != 0)
+    {
+        printf("Winsock Statup falied\n");
+        exit(1);
+    }    
+    return;
+}
+
+void Set_Local_Socket(void)
+{
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(SERVER_PORT);
+    memset(&(server_address.sin_zero),'\0', 8);
+    return;
+}
+
+
 int main (void)
 {
-    int teste = 10;
-    printf("%d", teste);
-
+    Start_Conection();
     return 0;
 }
